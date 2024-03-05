6e7df0e3125a877b995b71dd0217e6cf9740d5dd
e.demidova <kat.demidoffa@gmail.com>
2024-03-05 20:45:14 +0300

    Add project files.

---
+++ BrickOutService/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrickOutService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
